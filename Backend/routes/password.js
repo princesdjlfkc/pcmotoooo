8b2be26f7b390b18462d49a94e5ba598b3f27891
princesdjlfkc <princeramos231@gmail.com>
2026-02-14 22:02:23 +0800

    Delete Backend directory

--- Backend/routes/password.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { forgotPassword, resetPassword } = require('../controllers/passwordController');
-
-router.post('/forgot', forgotPassword);
-router.post('/reset', resetPassword);
-
-module.exports = router;
